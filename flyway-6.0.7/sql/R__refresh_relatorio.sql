0a329f681e476b8387f4f8f049a8d473aa9fdd64
Gustavo Kida <45009122+gustavokida@users.noreply.github.com>
2019-11-02 18:09:30 -0400

    Create R__refresh_relatorio.sql

---
+++ flyway-6.0.7/sql/R__refresh_relatorio.sql
@@ -0,0 +1,4 @@
+REFRESH MATERIALIZED VIEW relatorio_cliente_view;
+REFRESH MATERIALIZED VIEW relatorio_funcionario_view;
+REFRESH MATERIALIZED VIEW relatorio_view;
+REFRESH MATERIALIZED VIEW relatorio2_view;
