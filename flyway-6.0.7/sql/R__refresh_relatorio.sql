250d2490a3612280ed59643c851151b64894b08b
gustavokida <gustavokida@live.com>
2019-11-25 14:43:43 -0400

    deletado alguns arquivos

--- flyway-6.0.7/sql/R__refresh_relatorio.sql
+++
@@ -1,4 +0,0 @@
-REFRESH MATERIALIZED VIEW relatorio_cliente_view;
-REFRESH MATERIALIZED VIEW relatorio_funcionario_view;
-REFRESH MATERIALIZED VIEW relatorio_view;
-REFRESH MATERIALIZED VIEW relatorio2_view;
--- flyway-6.0.7/sql/V4__relatorio.sql
+++ flyway-6.0.7/sql/V4__relatorio.sql
@@ -1,61 +1,31 @@
 
-	--Seleciona clientes
-CREATE VIEW cliente_view AS
-SELECT clienteId, nome, quantidadeCompras, vip, sexo, endereco, dataNascimento
-FROM Cliente;
+  --Relatorio dos produtos comprados pelos clientes
+CREATE VIEW relatorio_view AS
+SELECT cv.nomeCliente, cv.data, cv.preco, cv.nomeFuncionario, mv.nomeMidia,
+mv.nomeGenero, mv.nomeTipo, pc.precoUnidade, pc.quantidade
+FROM ProdutosComprados pc
+LEFT OUTER JOIN compra_otimizada_view cv ON pc.fkCompraId = cv.compraId
+LEFT OUTER JOIN midia_otimizada_view mv ON pc.fkMidiaId = mv.midiaId
+ORDER BY cv.nomeCliente;
 
-	--Seleciona Compra
-CREATE VIEW compra_view AS
-SELECT compraId, data, preco, fkClienteId, fkFuncionarioId
-FROM Compra;
-
-	--Seleciona ProdutosComprados
-CREATE VIEW produtosComprados_view AS
-SELECT produtosCompradosId, fkCompraId, fkMidiaId, quantidade, precoUnidade, descontoUnidade
-FROM ProdutosComprados;
-
-	--Seleciona Midias
-CREATE VIEW midia_view AS
-SELECT tm.nome nomeTipo, m.midiaId, fkGeneroId, m.nome nomeMidia, m.dataPublicacao, m.idioma, m.localPublicacao, m.editora, m.precoMidia
-FROM Midia m, TipoMidia tm
-WHERE m.fkTipoMidiaId = tm.tipoMidiaId
-ORDER BY tm.nome;
-
-	--Seleciona genero
-CREATE VIEW genero_view AS
-SELECT generoId, nome, localizacao
-FROM Genero;
-
-	--SelecionaAutores e suas obras disponiveis na livraria
-CREATE VIEW autor_view AS
-SELECT  a.autorId, a.nome, a.nacionalidade, a.dataNascimento, a.dataFalecimento, am.fkMidiaId
-FROM Autor a, AutorMidia am
-WHERE a.autorId = am.fkAutorId
-ORDER BY a.nome;
-
-	--Seleciona Funcionarios
-CREATE VIEW funcionario_view AS
-SELECT funcionarioId, nome, funcao, salario, dataAdmissao
-FROM Funcionario;
+  --Relatorio de autores que tiveram suas midias vendidas e por quais funcionarios
+CREATE MATERIALIZED VIEW relatorio2_view AS
+SELECT amv.nome nomeAutor, amv.nomeMidia, amv.nomeTipo, amv.nomeGenero, 
+vfv.nome nomeFuncionario, vfv.data, vfv.quantidade
+FROM autor_midia_view amv
+INNER JOIN venda_funcionario_view vfv ON amv.midiaId = vfv.fkMidiaId
+ORDER BY nomeAutor;
 
 
-	--Seleciona cliente e suas compras - relatorio
-CREATE MATERIALIZED VIEW relatorio_cliente_view AS
-SELECT cv.nome AS nome1, cov.data, cov.preco, fv.nome AS nome2, mv.nomeMidia, gv.nome AS nome3, mv.nomeTipo, pcv.precoUnidade, pcv.quantidade
-FROM cliente_view cv
-LEFT OUTER JOIN compra_view cov ON cv.clienteId = cov.fkClienteId
-LEFT OUTER JOIN produtosComprados_view pcv ON cov.compraId = pcv.fkCompraId
-LEFT OUTER JOIN funcionario_view fv ON cov.fkFuncionarioId = fv.funcionarioId
-LEFT OUTER JOIN midia_view mv ON pcv.fkMidiaId = mv.midiaId
-LEFT OUTER JOIN genero_view gv ON mv.fkGeneroId = gv.generoId
-ORDER BY nome1;
+---Relatorio dos produtos comprados pelos clientes arrumado
 
-	--Quais autores tiveram midias vendidos por quais funcionarios - relatorio 2
-CREATE MATERIALIZED VIEW relatorio_funcionario_view AS
-SELECT av.nome AS nome1, mv.nomeMidia, mv.nomeTipo, fv.nome AS nome2, cov.data, pcv.quantidade
-FROM funcionario_view fv
-INNER JOIN compra_view cov ON fv.funcionarioId = cov.fkFuncionarioId
-INNER JOIN produtosComprados_view pcv ON pcv.fkCompraId = cov.compraId
-INNER JOIN midia_view mv ON pcv.fkMidiaId = mv.midiaId
-INNER JOIN autor_view av ON av.fkMidiaId = mv.midiaId
-ORDER BY nome1;
+CREATE VIEW relatorio_view AS
+SELECT
+
+
+
+
+---Relatorio de autores que tiveram suas midias vendidas e por quais funcionarios arrumado
+
+CREATE VIEW relatorio2_view AS
+SELECT
