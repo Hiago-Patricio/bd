eb388f5d6c5ce21c388de62da1346fda58bd2fac
gustavokida <gustavokida@live.com>
2019-11-26 16:20:25 -0400

    Arrumado relatorio

--- flyway-6.0.7/sql/V4__relatorio.sql
+++ flyway-6.0.7/sql/V4__relatorio.sql
@@ -6,10 +6,10 @@ SELECT c.nome as nomeCliente, co.data, co.preco, f.nome as nomeFuncionario, m.no
 g.nome as nomeGenero, tm.nome as nomeTipo, pc.precoUnidade, pc.quantidade
 FROM CLiente c
 LEFT OUTER JOIN Compra co ON c.clienteId = co.fkClienteId
-LEFT OUTER JOIN Funcionario f ON co.fkFuncionarioId = f.fkFuncionarioId
+LEFT OUTER JOIN Funcionario f ON co.fkFuncionarioId = f.funcionarioId
 LEFT OUTER JOIN ProdutosComprados pc ON co.compraId = pc.fkCompraId
 LEFT OUTER JOIN Midia m ON pc.fkMidiaId = m.midiaId
-LEFT OUTER JOIN TipoMidiaId tm ON m.TipoMidiaId = tm.tipoMidiaId
+LEFT OUTER JOIN TipoMidia tm ON m.fkTipoMidiaId = tm.tipoMidiaId
 LEFT OUTER JOIN Genero g ON m.fkGeneroId = g.generoId
 ORDER BY nomeCLiente;
 
@@ -23,10 +23,10 @@ SELECT a.nome as nomeAutor, m.nome as nomeMidia, tm.nome as nomeTipo, g.nome as 
 f.nome as nomeFuncionario, co.data, pc.quantidade
 FROM Autor a
 LEFT OUTER JOIN AutorMidia am ON a.autorId = am.fkAutorId
-LEFT OUTER JOIN Midia m ON am.fkMidiaId = m.fkMidiaId
+LEFT OUTER JOIN Midia m ON am.fkMidiaId = m.MidiaId
 LEFT OUTER JOIN Genero g ON m.fkGeneroId = g.generoId
 LEFT OUTER JOIN TipoMidia tm ON m.fkTipoMidiaId = tm.tipoMidiaId
 LEFT OUTER JOIN ProdutosComprados pc ON m.midiaId = pc.fkMidiaId
 LEFT OUTER JOIN Compra co ON pc.fkCompraId = co.compraId
 LEFT OUTER JOIN Funcionario f ON co.fkFuncionarioId = f.funcionarioId
-GROUP BY nomeAutor;
+ORDER BY nomeAutor;
