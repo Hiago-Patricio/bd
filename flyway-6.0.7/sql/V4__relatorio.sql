954fe910ddfed1575cad143cbc0d9666b7c83a0e
gustavokida <gustavokida@live.com>
2019-11-26 16:04:45 -0400

    Arrumado relatorios

--- flyway-6.0.7/sql/V4__relatorio.sql
+++ flyway-6.0.7/sql/V4__relatorio.sql
@@ -20,7 +20,16 @@ ORDER BY nomeAutor;
 ---Relatorio dos produtos comprados pelos clientes arrumado
 
 CREATE VIEW relatorio_view AS
-SELECT
+SELECT c.nome as nomeCliente, co.data, co.preco, f.nome as nomeFuncionario, m.nome as nomeMidia,
+g.nome as nomeGenero, tm.nome as nomeTipo, pc.precoUnidade, pc.quantidade
+FROM CLiente c
+LEFT OUTER JOIN Compra co ON c.clienteId = co.fkClienteId
+LEFT OUTER JOIN Funcionario f ON co.fkFuncionarioId = f.fkFuncionarioId
+LEFT OUTER JOIN ProdutosComprados pc ON co.compraId = pc.fkCompraId
+LEFT OUTER JOIN Midia m ON pc.fkMidiaId = m.midiaId
+LEFT OUTER JOIN TipoMidiaId tm ON m.TipoMidiaId = tm.tipoMidiaId
+LEFT OUTER JOIN Genero g ON m.fkGeneroId = g.generoId
+ORDER BY nomeCLiente;
 
 
 
@@ -28,4 +37,14 @@ SELECT
 ---Relatorio de autores que tiveram suas midias vendidas e por quais funcionarios arrumado
 
 CREATE VIEW relatorio2_view AS
-SELECT
+SELECT a.nome as nomeAutor, m.nome as nomeMidia, tm.nome as nomeTipo, g.nome as nomeGenero,
+f.nome as nomeFuncionario, co.data, pc.quantidade
+FROM Autor a
+LEFT OUTER JOIN AutorMidia am ON a.autorId = am.fkAutorId
+LEFT OUTER JOIN Midia m ON am.fkMidiaId = m.fkMidiaId
+LEFT OUTER JOIN Genero g ON m.fkGeneroId = g.generoId
+LEFT OUTER JOIN TipoMidia tm ON m.fkTipoMidiaId = tm.tipoMidiaId
+LEFT OUTER JOIN ProdutosComprados pc ON m.midiaId = pc.fkMidiaId
+LEFT OUTER JOIN Compra co ON pc.fkCompraId = co.compraId
+LEFT OUTER JOIN Funcionario f ON co.fkFuncionarioId = f.funcionarioId
+GROUP BY nomeAutor;
