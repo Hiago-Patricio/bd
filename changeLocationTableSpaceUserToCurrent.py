4ab0329c3f19e8a5a7b561c525d24207fb193bfa
Hiago Patricio <hiagodesousa13@gmail.com>
2019-12-08 10:17:43 -0400

    script para alterar o local do tablespace para o usuario atual

---
+++ changeLocationTableSpaceUserToCurrent.py
@@ -0,0 +1,33 @@
+import getpass
+
+def readFile(path):
+    with open(path, 'r') as f:
+        content = f.read()
+    return content
+
+
+def writeFile(path, content):
+    try:
+        with open(path, 'w+') as f:
+            f.write(content)
+        return True
+    except:
+        return False
+
+
+def changeUser(content):
+    return content.replace('~', '/home/' + getpass.getuser())
+
+
+filesToChange = [
+    'flyway-6.0.7/sql/V5__tablespace_livraria.sql',
+    'flyway-6.0.7/sql/V6__tablespace_indice.sql',
+]
+
+for file in filesToChange:
+    content = readFile(file)
+    content = changeUser(content)
+    if writeFile(file, content):
+        print('{} foi alterado com sucesso.'.format(file))
+    else:
+        print('{} não pode ser alterado.'.format(file))
