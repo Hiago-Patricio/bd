8e501f4fee95a25399a19b28c1137630770f59cf
hiago <hiagodesousa13@gmail.com>
2020-03-08 22:16:50 -0400

    envio de dados para table resumo local

--- example_spark.py
+++ example_spark.py
@@ -39,8 +39,20 @@ df = df.withColumn("vip", F.regexp_replace("vip", "false", "Não"))
 df = df.withColumn("vip", F.regexp_replace("vip", "true", "Sim"))
 
 
-df = df.select(F.col('quantidadecompras').alias("Quantidade de compras"), F.col('endereco').alias('Endereço'), F.col('sexo').alias('Sexo'), F.col('nome').alias('Nome'), F.col('datanascimento').alias("Data de Nascimento"), F.col('vip').alias('Vip'))
+df = df.select(F.col("clienteid").alias('Id'), F.col('quantidadecompras') \
+    .alias("Quantidade de compras"), F.col('endereco').alias('Endereço'), F.col('sexo') \
+    .alias('Sexo'), F.col('nome').alias('Nome'), F.col('datanascimento') \
+    .alias("Data de Nascimento"), F.col('vip').alias('Vip'))
 
-df = df.select('Quantidade de compras', 'Endereço', 'Sexo', 'Nome', getAge('Data de Nascimento').alias('Idade'), 'Vip')
+df = df.select('Id', 'Quantidade de compras', 'Endereço', 'Sexo', 'Nome', getAge('Data de Nascimento').alias('Idade'), 'Vip')
 
 df.show()
+
+df = df.write \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql:test") \
+    .option("dbtable", "resumo") \
+    .option("user", "postgres") \
+    .option("password", "123") \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
