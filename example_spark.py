b9b9fe86d2bcc83af17cd946c3d6b15e8c48e1cf
hiago <hiagodesousa13@gmail.com>
2020-03-08 21:48:07 -0400

    exemplo de pyspark criado

---
+++ example_spark.py
@@ -0,0 +1,46 @@
+import pyspark
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, functions as F, types as T
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+def getAgeYears(date: str) -> datetime:
+    date = str(date)
+    d1 = datetime.strptime(date, '%Y-%m-%d')
+    d2 = datetime.now()
+    diff = relativedelta(d2, d1)
+    return diff.years
+
+
+getAge = F.udf(getAgeYears, T.StringType())
+
+sc =SparkContext()
+sc.setLogLevel("Error")
+
+sqlContext = SQLContext(sc)
+
+# Seleciona quem tem John no nome ou sobrenome
+df = sqlContext.read \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql:test") \
+    .option("query", "SELECT * FROM public.cliente WHERE nome like '%John %' or nome like '%John' ORDER BY quantidadecompras DESC") \
+    .option("user", "postgres") \
+    .option("password", "123") \
+    .option("driver", "org.postgresql.Driver") \
+    .load()
+
+df.show()
+
+# Troca John por João, m por masculino e f por Feminino
+df = df.withColumn("nome", F.regexp_replace("nome", "John", "João"))
+df = df.withColumn("sexo", F.regexp_replace("sexo", "^M$", "Masculino"))
+df = df.withColumn("sexo", F.regexp_replace("sexo", "^F$", "Feminino"))
+df = df.withColumn("vip", F.regexp_replace("vip", "false", "Não"))
+df = df.withColumn("vip", F.regexp_replace("vip", "true", "Sim"))
+
+
+df = df.select(F.col('quantidadecompras').alias("Quantidade de compras"), F.col('endereco').alias('Endereço'), F.col('sexo').alias('Sexo'), F.col('nome').alias('Nome'), F.col('datanascimento').alias("Data de Nascimento"), F.col('vip').alias('Vip'))
+
+df = df.select('Quantidade de compras', 'Endereço', 'Sexo', 'Nome', getAge('Data de Nascimento').alias('Idade'), 'Vip')
+
+df.show()
